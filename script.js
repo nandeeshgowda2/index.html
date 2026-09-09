8fa312539a33b8c7ceaef5bc6a4297e2629027d7
nandeeshgowda2 <nandeeshgundalagadda@gmail.com>
2026-09-09 11:05:51 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+function startJourney() {
+
+    alert(
+        "Welcome to Track4Career! 🚀\n\n" +
+        "Your career journey starts here."
+    );
+
+}
+
+
+function applyInternship() {
+
+    alert(
+        "Internship opportunity selected!\n\n" +
+        "In the next version, you will be able to apply directly."
+    );
+
+}
